aa52878edf2515e1b6f90768d0789701bf52b468
Amartya Pardeshi <immortalforeigner.1@gmail.com>
2024-06-11 01:44:25 +0530

    first commit

---
+++ src/components/MovieList.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+const MovieList = (props) => {
+  const FavouriteComponent = props.favouriteComponent;
+  return (
+    <>
+      {props.movies.map((movie, index) => (
+        <div className="image-container justify-content-start m-3">
+          <img src={movie.Poster} alt="movie"></img>
+          <div
+            onClick={() => props.handleFavouritesClick(movie)}
+            className="overlay d-flex align-items-center justify-content-center"
+          >
+            <FavouriteComponent />
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default MovieList;
